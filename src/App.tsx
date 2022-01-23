06e3c633aab9f0926bc67905df082c0f0f5cd63d
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2022-01-23 12:39:01 -0300

    salvando dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import styled from 'styled-components';
+import { GlobalStyle } from "./styles/global";
 
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`
 
 export function App() {
   return (
     <div className="App">
-     <Title> hello world</Title>
+    <h1> Hello World</h1>
+    <GlobalStyle />
     </div>
   );
 }

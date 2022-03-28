8197bb26f23bb72e9b486e96b56b23fd2a8f67f3
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2022-03-28 14:31:03 -0300

    save

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,19 @@
+import { Container } from "../Summary/styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+      </table>
+    </Container>
+  );
+}
